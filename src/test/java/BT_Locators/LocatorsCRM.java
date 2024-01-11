9aa48bf610c2aa09aaa4ede00edbd54c0505464e
dangngocanh106 <155122413+dangngocanh106@users.noreply.github.com>
2024-01-11 11:14:48 +0700

    Bai tap Selenium

---
+++ src/test/java/BT_Locators/LocatorsCRM.java
@@ -0,0 +1,31 @@
+package BT_Locators;
+
+public class LocatorsCRM {
+
+    //1. Khai báo các locator trên trang Login
+    private String email;
+    private String password;
+    private String buttonLogin;
+    private String checkboxRemember;
+    private String linkForgotPassword;
+
+    //2. Khai báo các locator trên trang Customer
+    private String menuCustomer;
+
+    //3. Khai báo các locator trên trang add new customer
+    private String company;
+    private String vatNumber;
+    private String phone;
+    private String website;
+    private String comboboxGroups;
+    private String comboboxCurrency;
+    private String comboboxDefaultLanguage;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String comboboxCountry;
+    private String buttonSaveAndCreate;
+    private String buttonSave;
+
+}
